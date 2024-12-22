f165578b64ec7466d004dbae93257e7ec17a86cb
unknown <m.parceiller@free.fr>
2024-12-22 21:10:51 +0100

    add dynamism component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,27 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
-    return <figure> 
-        <img 
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
-        </img>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
+    // Récupère le premier Pokémon du tableau
+    const pokemon = pokemonList[1];
+
+    return (
+        <figure>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
 }
 
 export default PokemonCard;
